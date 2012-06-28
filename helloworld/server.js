a81eac36ae57e52b25670473d9937f6d9207d0d4
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-06-28 16:48:09 -0700

    jshint files

--- helloworld/server.js
+++ helloworld/server.js
@@ -1,4 +1,5 @@
 require('http').createServer(function (request, response) {
+  'use strict';
   response.writeHead(200, {"Content-Type": "text/plain"});
   output = "Hello Dotcloud from Wercker!\n";
   for (k in request.headers) {
