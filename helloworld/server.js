fc3ca7530d772d3f55f2e15d4c1a811a97c25957
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-06-28 16:51:20 -0700

    fixed errors

--- helloworld/server.js
+++ helloworld/server.js
@@ -1,8 +1,8 @@
 require('http').createServer(function (request, response) {
   'use strict';
   response.writeHead(200, {"Content-Type": "text/plain"});
-  output = "Hello Dotcloud from Wercker!\n";
-  for (k in request.headers) {
+  var output = "Hello Dotcloud from Wercker!\n";
+  for (var k in request.headers) {
     output += k + '=' + request.headers[k] + '\n';
   }
   response.end(output);
